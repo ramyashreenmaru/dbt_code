bd0786ce54f2e76a88ced613556ad27b7b1448ec
ramya75789@gmail.com <ramya75789@gmail.com>
2024-09-19 12:01:38 +0000

    COMMIT

---
+++ models/staging/orders.sql
@@ -0,0 +1,13 @@
+{{  config(
+    materialized = 'view')
+}}
+
+WITH tb1 as(
+    select id,
+    order_Date,
+    user_id
+
+    from {{source('datafeed_Shared_schema','STG_ORDER')}})
+    select * From tb1;
+
+)
